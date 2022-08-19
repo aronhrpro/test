5a61d3008fe5ee2265bc68cfd7786ed88bceba87
Aron Margetić <56715397+aronhrpro@users.noreply.github.com>
2022-08-19 08:15:56 +0200

    Add files via upload

---
+++ demo_02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace demo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Readline();
+            Console.WriteLine("kraj");
+        }
+    }
+}
